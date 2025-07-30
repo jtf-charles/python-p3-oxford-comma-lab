73b2c4f6902f881b593b608dece0ef024ca5b5d3
unknown <jtf-charles>
2025-07-30 00:35:26 -0400

    lab completed

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,11 @@
 def oxford_comma(items):
-    return None
+    if len(items)==1:
+        return items[0]
+    elif len(items)==2:
+        return items[0]+" and "+ items[1]
+    else:
+        S=", ".join(items[:-1])
+        S=S+", and "+items[-1]
+        #print(S)
+        return S
+#oxford_comma(["fiddleheads", "okra", "kohlrabi"])
